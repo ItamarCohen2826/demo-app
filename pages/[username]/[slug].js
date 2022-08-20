b5c0f300071e5c0b847ddf7439c64339a435adc1
ItamarCohen2826 <itamar.cohen2826@gmail.com>
2022-08-20 17:33:01 +0300

    time to ship it

--- pages/[username]/[slug].js
+++ pages/[username]/[slug].js
@@ -5,76 +5,86 @@ import MetaTags from "../../components/Metatags";
 import AuthCheck from '../../components/AuthCheck';
 import Link from 'next/link';
 import Heart from '../../components/HeartButton';
+import { useContext } from 'react';
+import { UserContext } from '../../lib/context';
 
 export async function getStaticProps({ params }) {
     const { username, slug } = params;
     const userDoc = await getUserWithUsername(username);
-    if (!userDoc) {
-        return {
-            notFound: true
-        }
-    }
+  
     let post;
     let path;
-
+  
     if (userDoc) {
-        const postRef = userDoc.ref.collection('posts').doc(slug);
-        post = postToJSON(await postRef.get());
-
-        path = postRef.path;
+      const postRef = userDoc.ref.collection('posts').doc(slug);
+      post = postToJSON(await postRef.get());
+  
+      path = postRef.path;
     }
-
+  
     return {
-        props: { post, path },
-        revalidate: 5000,
-    }
-}
-
-export async function getStaticPaths() {
-    const snapshot = await firestore.collectionGroup('posts').get();
-    const paths = snapshot.docs.map((doc) => {
-        const { slug, username } = doc.data();
-        // The slug should be the same as the document name for the paths to work!
-        return {
-             params : { username, slug }
-        };
-    });
-    return {
-        /*
-        Must be in this format:
-        paths: [
-            { params: { username, slug }}
-        ],
-         */
-        paths,
-        fallback: 'blocking',
+      props: { post, path },
+      revalidate: 100,
     };
-}
-
-export default function Post(props) {
+  }
+  
+  export async function getStaticPaths() {
+    // Improve my using Admin SDK to select empty docs
+    const snapshot = await firestore.collectionGroup('posts').get();
+  
+    const paths = snapshot.docs.map((doc) => {
+      const { slug, username } = doc.data();
+      return {
+        params: { username, slug },
+      };
+    });
+  
+    return {
+      // must be in this format:
+      // paths: [
+      //   { params: { username, slug }}
+      // ],
+      paths,
+      fallback: 'blocking',
+    };
+  }
+  
+  export default function Post(props) {
     const postRef = firestore.doc(props.path);
     const [realtimePost] = useDocumentData(postRef);
   
     const post = realtimePost || props.post;
+  
+    const { user: currentUser } = useContext(UserContext);
+  
     return (
-        <main>
-            <MetaTags title="Post" />
-            <section>
-                <PostContent post={post} />
-            </section>            
-            <aside className="card" />
-            <p>
-                <strong>{post.heartCount || 0} ❤️</strong>
-            </p>
-        <AuthCheck
+      <main>
+        <MetaTags title="Post" />      
+        <section>
+          <PostContent post={post} />
+        </section>
+  
+        <aside className="card">
+          <p>
+            <strong>{post.heartCount || 0} 🤍</strong>
+          </p>
+  
+          <AuthCheck
             fallback={
-            <Link href="/enter">
-              <button>💗 Sign Up</button>
+              <Link href="/enter">
+                <button>💗 Sign Up</button>
+              </Link>
+            }
+          >
+            <Heart postRef={postRef} />
+          </AuthCheck>
+  
+          {currentUser?.uid === post.uid && (
+            <Link href={`/admin/${post.slug}`}>
+              <button className="btn-blue">Edit Post</button>
             </Link>
-          }
-        >
-          <Heart postRef={postRef} />
-        </AuthCheck>
-        </main>
-    )
-}
+          )}
+        </aside>
+      </main>
+    );
+  }
